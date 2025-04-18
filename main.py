fd8f52e0390fb48b0fe979c959023a1a9c9bf6c2
SwAt1563 <swat.ar123@gmail.com>
2025-04-18 18:00:18 +0300

    update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello Candidate!"}
 
-A = LLM()
 
 # TODO: create the new endpoint here
 # The payload should have, the text and the model name
